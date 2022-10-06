f3413d41014b9ecc6e3531577bac37aa4b2ecaac
Rodriguez <sergerodriguez.education@gmail.com>
2022-10-06 10:38:35 +0500

    feat: add new error messages

--- errors_message.py
+++ errors_message.py
@@ -7,7 +7,10 @@ errors = {
     'S006': 'More than two blank lines used before this line',
     'S007': 'Too many spaces after "{}"',
     'S008': 'Class name "{}" should be written in CamelCase',
-    'S009': 'Function name "{}" should be written in snake_case'
+    'S009': 'Function name "{}" should be written in snake_case',
+    'S010': 'Argument name {} should be written in snake_case',
+    'S011': 'Variable {} should be written in snake_case',
+    'S012': 'The default argument value is mutable'
 }
 
 
